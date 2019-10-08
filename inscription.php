1183e9574254144a0731351a5f426dfa082081c8
Jonathan Guazzelli <jonathanguazzelli@hotmail.fr>
2019-10-08 15:17:35 +0200

    forumulaire_inscription

--- inscription.php
+++ inscription.php
@@ -1,3 +1,71 @@
+<?php
+
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=pfmembre', 'root', '');
+
+if(isset($_POST['forminscription']))
+{
+	$pseudo = htmlspecialchars($_POST['pseudo']);
+	$mail = htmlspecialchars($_POST['mail']);
+	$mail2 = htmlspecialchars($_POST['mail2']);
+	$mdp = sha1($_POST['mdp']);
+	$mdp2 = sha1($_POST['mdp2']);
+
+	if(!empty($_POST['pseudo']) AND !empty($_POST['mail']) AND !empty($_POST['mail2']) AND !empty($_POST['mdp']) AND !empty($_POST['mdp2']))
+	{
+
+
+		$pseudolenght = strlen($pseudo);
+		if($pseudolenght <= 255)
+		{
+			
+				if($mail == $mail2)
+				{
+					if(filter_var($mail, FILTER_VALIDATE_EMAIL))
+					{
+						$reqmail = $bdd->prepare("SELECT * FROM membres WHERE mail = ?");
+						$reqmail->execute(array($mail));
+						$mailexist = $reqmail->rowCount();
+						if($mailexist == 0)
+						{
+							if($mdp == $mdp2)
+							{
+								$insertmbr = $bdd->prepare("INSERT INTO membres(pseudo, mail, mdp)  VALUES(?, ?, ?)");
+								$insertmbr->execute(array($pseudo, $mail, $mdp));
+								$erreur = "Votre compte a bien été créé";
+							}
+							else
+							{
+								$erreur = "Vos mots de passe sont différents.";
+							}
+						}
+						else
+						{
+							$erreur = "Adresse mail déja utilisée.";
+						}	
+
+					}	
+					else
+					{
+						$erreur = "Votre adresse mail n'est pas valide.";
+					}	
+				}
+				else
+				{
+					$erreur = "Vos adresses mails ne correspondent pas.";
+				}
+		}
+		else
+		{
+			$erreur = "Votre pseudo ne doit pas dépasser 255 caractères.";
+		}
+	}
+	else{
+		$erreur = "Tous les champs doivent être complétés";
+	}
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -29,7 +97,7 @@
 							<label for="pseudo">Pseudo :</label>
 						</td>	
 						<td>	
-							<input type="text" placeholder="Votre pseudo" id="pseudo" name="pseudo">
+							<input type="text" placeholder="Votre pseudo" id="pseudo" name="pseudo" value="<?php if(isset($pseudo)) { echo $pseudo; } ?>">
 						</td>
 					</tr>
 					<tr>
@@ -37,7 +105,7 @@
 							<label for="mail">E-Mail :</label>
 						</td>	
 						<td>	
-							<input type="email" placeholder="Votre e-mail" id="mail" name="mail">
+							<input type="email" placeholder="Votre e-mail" id="mail" name="mail" value="<?php if(isset($mail)) { echo $mail; } ?>">
 						</td>
 					</tr>
 					<tr>
@@ -45,7 +113,7 @@
 							<label for="mail2">Confirmez votre e-mail :</label>
 						</td>	
 						<td>	
-							<input type="email2" placeholder="Votre e-mail" id="mail2" name="mail2">
+							<input type="email2" placeholder="Votre e-mail" id="mail2" name="mail2" value="<?php if(isset($mail2)) { echo $mail2; } ?>">
 						</td>
 					</tr>
 					<tr>
@@ -68,10 +136,19 @@
 						<td></td>
 						<td>
 							<br />
-							<input type="submit" value="S'inscrire"	>
+							<input type="submit" name="forminscription" value="S'inscrire"	>
 						</td>
 					</tr>
 				</table>
+			</form>	
+			<?php 
+
+			if(isset($erreur)){
+				echo $erreur;
+			}
+
+
+			?>
 					
 			</div>
 		  </div>
